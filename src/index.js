d845869ebfddece65844e17377f31451636d9657
John J Rodriguez <87795271+JohnRodriguezM@users.noreply.github.com>
2023-10-23 22:57:37 -0500

    cors config

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const morgan = require("morgan");
 const { v4: uuidv4 } = require("uuid");
+const cors = require("cors");
 
 //* local imports
 const taskRouter = require("./routes/task.routes");
 
 const app = express();
 
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 //* routes definition
 
