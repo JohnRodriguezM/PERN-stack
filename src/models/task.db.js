2e1ec7ba12f072ee1bcbcfc4f6eae733f5fafc82
John J Rodriguez <87795271+JohnRodriguezM@users.noreply.github.com>
2023-10-23 22:53:32 -0500

    .env vars

--- src/models/task.db.js
+++ src/models/task.db.js
@@ -1,12 +1,8 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  user: "postgres",
-  password: "0906",
-  host: "localhost",
-  port: 5432,
-  database: "taskdb",
-});
+const { db } = require("../config");
+
+const pool = new Pool(db);
 
 pool.connect((err, client, release) => {
   if (err) {
